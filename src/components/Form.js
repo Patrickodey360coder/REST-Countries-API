21a329cc1d4ceb723bc1e88265bacac9f8d47529
patrickodey <patrickodey419@gmail.com>
2021-10-03 04:53:21 +0100

    form

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,9 @@ function Form() {
   return (
     <div className="row">
       <div className="col-md-6">
-      <div class="form-group">
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-        <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-      </div>
+        <div class="form-group">
+          <input type="text" class="form-control" placeholder="Search for a country..." />
+        </div>
       </div>
       <div className="col-md-6">
 
